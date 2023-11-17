18b69e99c3f5dab999598a81a37e5ceb3b5c3629
gruvector <vector.gru316@gmail.com>
2023-11-17 22:50:20 +0900

    update_order1

--- resources/views/pages/order.blade.php
+++ resources/views/pages/order.blade.php
@@ -68,49 +68,66 @@
             
         </div>
     </div>
-     
-        <div class="container mt-3">
-        <h2>Dynamic Tabs</h2>
-        <br>
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-            <a class="nav-link active" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#menu1">Menu 1</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#menu2">Menu 2</a>
-            </li>
-        </ul>
-
-        <!-- Tab panes -->
-        <div class="tab-content">
-            <div id="home" class="container tab-pane active"><br>
-            <h3>HOME</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div id="menu1" class="container tab-pane fade"><br>
-            <h3>Menu 1</h3>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-            <div id="menu2" class="container tab-pane fade"><br>
-            <h3>Menu 2</h3>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-            </div>
-        </div>
-        </div>
-
 </div>
 
 {{-- Header End --}}
 
-{{-- Merit Start --}}
+{{-- Tab Start --}}
 
 
 
-{{-- Merit End --}}
+    <!-- Nav tabs -->
+
+<div class="order_tab">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu1">Menu 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu3">Menu 3</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu4">Menu 4</a>
+        </li>
+    </ul>
+  
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div id="home" class=" tab-pane active"><br>
+            <div class="order_tab_part1">
+
+            </div>
+            <div class="order_tab_part2">
+                dfdfsef
+            </div>
+        </div>
+        <div id="menu1" class=" tab-pane fade"><br>
+            <h3>Menu 1</h3>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div id="menu2" class=" tab-pane fade"><br>
+            <h3>Menu 2</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+        <div id="menu3" class=" tab-pane fade"><br>
+            <h3>Menu 2</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+        <div id="menu4" class=" tab-pane fade"><br>
+            <h3>Menu 2</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+    </div>
+</div>
+
+
+{{-- Tab End --}}
 
 {{-- Electronic commerce Start --}}
 
